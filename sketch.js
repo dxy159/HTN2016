ad27a23853f94337886a3e95998e1e9c8ee4ffee
jedidreyfus <sarkr16@gmail.com>
2016-09-17 00:18:03 -0400

    Added the skeleton the of the Boid class

--- sketch.js
+++ sketch.js
@@ -1,7 +1,35 @@
 function setup() {
-
+  createCanvas(600,600);
 }
 
 function draw() {
-  
+
 }
+
+function Boid() {
+
+}
+
+Boid.prototype.update = function () {
+
+};
+
+Boid.prototype.draw = function () {
+
+};
+
+Boid.prototype.attract = function () {
+
+};
+
+Boid.prototype.cohesion = function () {
+
+};
+
+Boid.prototype.separation = function () {
+
+};
+
+Boid.prototype.allignment = function () {
+  
+};
