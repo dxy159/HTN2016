24bd8f16d7a312a43635a0f58116fbf25f76b8ab
dxy159 <richard.ni.wchs@gmail.com>
2016-09-17 01:13:54 -0400

    Implemented get methods for p1 p2 and p3

--- sketch.js
+++ sketch.js
@@ -1,12 +1,36 @@
 function setup() {
-  createCanvas(600,600);
+	//create canvas
+	createCanvas(720, 400);
+	
 }
 
 function draw() {
-
+	//set background colour
+  	background(0);
+  	//fill colour
+  	fill(0, 180, 180);
 }
 
+function rotate() {
+	rotate(PI/3.0);
+}
+
+draw();
+rotate();
+
 function Boid() {
+
+	get p1() {
+		return createVector(this.pos.x - 6, this.pos.y - 10);
+	}
+
+	get p2() {
+		return createVector(this.pos.x, this.pos.y + 10);
+	}
+
+	get p3() {
+		return createVector(this.pos.x + 6, this.pos.y - 10);
+	}
 
 }
 
@@ -15,7 +39,8 @@ Boid.prototype.update = function () {
 };
 
 Boid.prototype.draw = function () {
-
+	fill(Math.random() * 255);
+	triangle(30, 75, 36, 55, 42, 75);
 };
 
 Boid.prototype.attract = function () {
